b89235738d53b7e4c32a7fff7ff1c004dbe46af9
Nicolas Sotelo <nicolassotelo@Nicolass-MacBook-Air.local>
2019-11-12 19:53:42 -0500

    final

---
+++ precache-manifest.45eaccbdb9dfe0ed9e5dfd72b3405eac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "441aee07573e122a8aecfdb62bce2571",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d00084f7718f15a56764",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "8df4c7a885cd2b1a0338",
+    "url": "/static/js/2.7e7df7d4.chunk.js"
+  },
+  {
+    "revision": "d00084f7718f15a56764",
+    "url": "/static/js/main.6421a84d.chunk.js"
+  },
+  {
+    "revision": "beb01a0586cde79a8fb5",
+    "url": "/static/js/runtime-main.303d1844.js"
+  }
+]);
